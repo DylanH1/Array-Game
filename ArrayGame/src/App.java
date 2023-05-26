d882ae85b4e50f6b2c1a20b198acca6907fabd59
DylanH1 <125473612+DylanH1@users.noreply.github.com>
2023-05-26 08:47:50 -0400

    Major Wipe

--- ArrayGame/src/App.java
+++ ArrayGame/src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        ArrayUtils.prn(" Which number would you like to move?");
+        int input2 = sc.nextInt();
+        int playerSprite = input2;
+        int rowMove = 0;
+        int colMove = 0;
+        String input = sc.nextLine();
+        if (input.equals("up")) {
+            rowMove--;
+        }
+        else if (input.equals("down")) {
+            rowMove++;
+        }
+        else if (input.equals("left")) {
+            colMove--;
+        }
+        else if (input.equals("right")) {
+            colMove++;
+        }
+
+    }
+
+    public static int[][] loadMap() {
+        // TODO: Load from a file.
+        int[][] map = {
+                { 2, 4, 5 },
+                { 7, 6, 9 },
+                { 3, 8, 1 },         
+        };
+        System.out.println(map);
+        return map;
+    }
+    public static void display(int[][] map, String[] tiles, String playerSprite, int playerRow, int playerCol) {
+
+        // TODO: Display borders around map.
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (i == playerRow && j == playerCol) { // Player
+                    System.out.print(playerSprite);
+                }
+                else {  // Display correct tile according to tile code in game map.
+                    int tileCode = map[i][j];
+                    System.out.print(tiles[tileCode]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
---
+++ ArrayGame/src/ArrayUtils.java
@@ -0,0 +1,109 @@
+public class ArrayUtils {
+    /**
+     * Overloaded versions of printArray
+     * Prints specified array, one element per line.
+     */
+    public static void printArray(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    public static void printArray(long[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    public static void printArray(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    public static void printArray(char[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    public static void printArray(boolean[] array) {
+        for (int i = 0; i < array.length; i++) {
+            prn(array[i]);
+        }
+    }
+
+    /**
+     * Overloaded versions of createFilledArray.
+     * Creates and returns an array of specified size, filled with specified value.
+     */
+    public static double[] createFilledArray(int size, double value) {
+        double[] array = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static float[] createFilledArray(int size, float value) {
+        float[] array = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static long[] createFilledArray(int size, long value) {
+        long[] array = new long[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static int[] createFilledArray(int size, int value) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static char[] createFilledArray(int size, char value) {
+        char[] array = new char[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static boolean[] createFilledArray(int size, boolean value) {
+        boolean[] array = new boolean[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = value;
+        }
+
+        return array;
+    }
+
+    public static <T> void prn( T text){
+        System.out.println(text);
+    }
+}
