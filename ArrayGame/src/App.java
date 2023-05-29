9cdc5475b78d16a05131d0cbf71f763950edb768
DylanH1 <125473612+DylanH1@users.noreply.github.com>
2023-05-29 08:22:43 -0400

    Forgot to update, have not implemented methods

--- ArrayGame/src/App.java
+++ ArrayGame/src/App.java
@@ -3,52 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        ArrayUtils.prn(" Which number would you like to move?");
-        int input2 = sc.nextInt();
-        int playerSprite = input2;
-        int rowMove = 0;
-        int colMove = 0;
-        String input = sc.nextLine();
-        if (input.equals("up")) {
-            rowMove--;
-        }
-        else if (input.equals("down")) {
-            rowMove++;
-        }
-        else if (input.equals("left")) {
-            colMove--;
-        }
-        else if (input.equals("right")) {
-            colMove++;
-        }
-
-    }
-
-    public static int[][] loadMap() {
-        // TODO: Load from a file.
+        boolean done = false;
         int[][] map = {
-                { 2, 4, 5 },
-                { 7, 6, 9 },
-                { 3, 8, 1 },         
-        };
-        System.out.println(map);
-        return map;
+            { 1, 2, 3,},
+            { 4, 5, 6,},
+            { 7, 8, 0,},
+            
+    };
+        while (!done) {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    ArrayUtils.prn( map[i][j]);
+        }   
     }
-    public static void display(int[][] map, String[] tiles, String playerSprite, int playerRow, int playerCol) {
-
-        // TODO: Display borders around map.
-
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (i == playerRow && j == playerCol) { // Player
-                    System.out.print(playerSprite);
-                }
-                else {  // Display correct tile according to tile code in game map.
-                    int tileCode = map[i][j];
-                    System.out.print(tiles[tileCode]);
-                }
-            }
-            System.out.println();
-        }
+}
     }
 }
